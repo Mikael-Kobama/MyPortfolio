2af6127c251eec9853dbba594fcd810a114a2673
Sr. Kobama <125419153+Mikael-Kobama@users.noreply.github.com>
2025-02-25 22:14:30 -0300

    Send Email Update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,48 +1,64 @@
+// Função para mostrar o toast
+function showToast(message, type) {
+  const toast = document.getElementById("responseMessage");
+  toast.textContent = message;
+  toast.classList.remove("success", "error");
+  toast.classList.add(type, "show");
+
+  // Após 3 segundos, escondemos o toast
+  setTimeout(() => {
+    toast.classList.remove("show");
+  }, 3000); // 3 segundos
+}
+
 /*===== MENU SHOW =====*/
 const showMenu = (toggleId, navId) => {
-  const toggle = document.getElementById(toggleId),
-    nav = document.getElementById(navId);
+  const toggle = document.getElementById(toggleId);
+  const nav = document.getElementById(navId);
 
-  if (toggle && nav) {
-    toggle.addEventListener("click", () => {
-      nav.classList.toggle("show");
-    });
-  }
+  if (!toggle || !nav) return; // Retorna se qualquer um dos elementos não for encontrado
+
+  toggle.addEventListener("click", () => {
+    nav.classList.toggle("show");
+  });
 };
 showMenu("nav-toggle", "nav-menu");
 
 /*==================== REMOVE MENU MOBILE ====================*/
-const navLink = document.querySelectorAll(".nav__link");
-
-function linkAction() {
-  const navMenu = document.getElementById("nav-menu");
-  // When we click on each nav__link, we remove the show-menu class
-  navMenu.classList.remove("show");
-}
-navLink.forEach((n) => n.addEventListener("click", linkAction));
+document.querySelectorAll(".nav__link").forEach((link) => {
+  link.addEventListener("click", () => {
+    document.getElementById("nav-menu").classList.remove("show");
+  });
+});
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 const sections = document.querySelectorAll("section[id]");
 
 const scrollActive = () => {
-  const scrollDown = window.scrollY;
+  const scrollPosition = window.scrollY;
 
-  sections.forEach((current) => {
-    const sectionHeight = current.offsetHeight,
-      sectionTop = current.offsetTop - 58,
-      sectionId = current.getAttribute("id"),
-      sectionsClass = document.querySelector(
-        ".nav__menu a[href*=" + sectionId + "]"
-      );
+  sections.forEach((section) => {
+    const { offsetHeight, offsetTop } = section;
+    const sectionId = section.getAttribute("id");
+    const sectionLink = document.querySelector(
+      `.nav__menu a[href*=${sectionId}]`
+    );
 
-    if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
-      sectionsClass.classList.add("active-link");
+    // Verifica se a seção está visível na tela
+    if (
+      scrollPosition >= offsetTop - 58 &&
+      scrollPosition <= offsetTop + offsetHeight
+    ) {
+      sectionLink.classList.add("active-link");
     } else {
-      sectionsClass.classList.remove("active-link");
+      sectionLink.classList.remove("active-link");
     }
   });
 };
-window.addEventListener("scroll", scrollActive);
+
+window.addEventListener("scroll", () => {
+  scrollActive();
+});
 
 /*===== SCROLL REVEAL ANIMATION =====*/
 const sr = ScrollReveal({
@@ -60,17 +76,30 @@ sr.reveal(".home__img, .about__subtitle, .about__text, .skills__img", {
 sr.reveal(".home__social-icon", { interval: 200 });
 sr.reveal(".skills__data, .work__img, .contact__input", { interval: 200 });
 
+/*===== SEND FORM =====*/
+// Inicialize o EmailJS com sua chave pública
+emailjs.init("OH-gvD1TmasV1thFV");
+
+// Adiciona o evento de envio do formulário
 document
-  .getElementById("send-button")
-  .addEventListener("click", async function () {
-    let form = document.getElementById("contact-form");
-    let formData = new FormData(form);
+  .getElementById("contact-form")
+  .addEventListener("submit", function (event) {
+    event.preventDefault(); // Impede o envio padrão do formulário
 
-    let response = await fetch("./php/send_email.php", {
-      method: "POST",
-      body: formData,
-    });
+    // Coleta os dados do formulário
+    const formData = new FormData(this);
 
-    let result = await response.text();
-    document.getElementById("responseMessage").innerText = result; // Exibe a resposta do servidor
+    // Envia o e-mail com os dados do formulário usando EmailJS
+    emailjs.sendForm("service_wi886xj", "template_hbr8a31", this).then(
+      function (response) {
+        console.log("Sucesso:", response);
+        // Exibe a mensagem de sucesso
+        showToast("Mensagem enviada com sucesso!", "success");
+      },
+      function (error) {
+        console.log("Erro:", error);
+        // Exibe a mensagem de erro
+        showToast("Ocorreu um erro. Tente novamente.", "error");
+      }
+    );
   });
